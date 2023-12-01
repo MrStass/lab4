c5fe193466e5f045d9e811d48edc7d7c101c9573
stasi <stasikpyskir2@gmail.com>
2023-12-01 17:25:51 +0200

    дфи4

---
+++ src/app/recipe.model.ts
@@ -0,0 +1,14 @@
+export class Recipe {
+      id: number;
+      name: string;
+      description: string;
+      ingredients: string;
+  
+      constructor(id: number, name: string, description: string, ingredients: string) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.ingredients = ingredients;
+      }
+    }
+  
---
+++ src/app/recipe.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  private readonly apiHost = 'http://localhost:5267';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getRecipes(): Observable<Recipe[]> {
+    return this.httpClient.get<any>(`${this.apiHost}/recipes`)
+      .pipe(
+        map(response => (response.data as any[])
+          .map(recipe => new Recipe(recipe.id, recipe.name, recipe.description, recipe.ingredients)))
+      );
+  }
+}
